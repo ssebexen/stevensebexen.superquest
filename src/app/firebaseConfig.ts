423009777c2bc8d2db53d54980dce4df12547bf5
Steven <ssebexen@gmail.com>
2024-07-02 09:30:19 -0400

    API key isn't secret.

---
+++ src/app/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQMzaVlDPp55e_69TndDawbEBjdGL4HNc",
+  authDomain: "super-quest.firebaseapp.com",
+  projectId: "super-quest",
+  storageBucket: "super-quest.appspot.com",
+  messagingSenderId: "967651742908",
+  appId: "1:967651742908:web:034ed97e7afcfb86036cb9",
+  measurementId: "G-1BYERRZLHG"
+};
+
+export default firebaseConfig;
